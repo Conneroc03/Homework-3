aa18a3ba625bbd2f9db8bb78ef30d2c03ab0b71e
Conneroc03 <122755260+Conneroc03@users.noreply.github.com>
2023-03-13 16:02:25 -0600

    added the pop ups for passwork criteria

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
+
+
+
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+var passwordLength = prompt("How many characters would you like your password to contain?");
+var passwordLength = parseInt(passwordLength);
+var passwordLowercase = confirm("Click OK to confirm including lowercase characters.");
+var passwordUppercase = confirm("Click OK to confirm including uppercase characters.");
+var passwordNumeric = confirm("Click OK to confirm including numeric characters.");
+var passwordSpecial = confirm("Click OK to confirm including special characters.");
+var password = "";
+var passwordCharacters = "";
+var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericCharacters = "0123456789";
+var specialCharacters = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+var randomLowercase = lowercaseCharacters[Math.floor(Math.random() * lowercaseCharacters.length)];
+var randomUppercase = uppercaseCharacters[Math.floor(Math.random() * uppercaseCharacters.length)];
+var randomNumeric = numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
+var randomSpecial = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+
