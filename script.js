905b3990a47ad914634cb8cfbe7d977c806fec58
Conneroc03 <122755260+Conneroc03@users.noreply.github.com>
2023-03-13 17:27:41 -0600

    fix

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 var generateBtn = document.querySelector("#generate");
 
 function GeneratePassword() {
-  var password = generatePassword();
+  function GeneratePassword() {
+    var passwordLength = prompt(
+      "How many characters would you like your password to contain?");
+    var passwordLength = parseInt(passwordLength);
+    var passwordLowercase = confirm(
+      "Click OK to confirm including lowercase characters.");
+    var passwordUppercase = confirm(
+      "Click OK to confirm including uppercase characters.");
+    var passwordNumeric = confirm(
+      "Click OK to confirm including numeric characters.");
+    var passwordSpecial = confirm(
+      "Click OK to confirm including special characters.");
+  }
+  var password = GeneratePassword();
   var passwordText = document.querySelector("#password");
   if (passwordLength < 8) {
     alert("Password must be at least 8 characters");
@@ -10,43 +23,29 @@ function GeneratePassword() {
     alert("Password must be less than 128 characters");
   }
   if (passwordLength >= 8 && passwordLength <= 128) {
-    alert("Password must be between 8 and 128 characters");
+    alert('password must be between 8 and 128 characters');
   }
   if (passwordLowercase === false && passwordUppercase === false && passwordNumeric === false && passwordSpecial === false) {
-    alert("You must select at least one character type");
+    alert('You must select at least one character type');
   }
-  for (let i = 1; i < passwordLength; i++) {
-    var char = passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-    var password = "";
-    var passwordCharacters = "";
-    var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
-    var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var numericCharacters = "0123456789";
-    var specialCharacters = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
-    passwordCharacters += randomLowercase;
-    
-  }
-    class generatePassword {
-      constructor(passwordLength, passwordLowercase, passwordUppercase, passwordNumeric, passwordSpecial) {
+  class generatePasswordClass {
+    constructor(
+        passwordLength, 
+        passwordLowercase,
+        passwordUppercase,
+        passwordNumeric, 
+        passwordSpecial 
+    ) {
         this.passwordLength = passwordLength;
         this.passwordLowercase = passwordLowercase;
         this.passwordUppercase = passwordUppercase;
         this.passwordNumeric = passwordNumeric;
         this.passwordSpecial = passwordSpecial;
-      }
     }
-
-
-
+  }
 
   passwordText.value = password;
 
 }
 
 generateBtn.addEventListener("click", GeneratePassword);
-var passwordLength = prompt("How many characters would you like your password to contain?");
-var passwordLength = parseInt(passwordLength);
-var passwordLowercase = confirm("Click OK to confirm including lowercase characters.");
-var passwordUppercase = confirm("Click OK to confirm including uppercase characters.");
-var passwordNumeric = confirm("Click OK to confirm including numeric characters.");
-var passwordSpecial = confirm("Click OK to confirm including special characters.");
