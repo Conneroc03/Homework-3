7fb899b199fbdefb327ef81c1e8feaf544ff5a13
Conneroc03 <122755260+Conneroc03@users.noreply.github.com>
2023-03-13 16:55:20 -0600

    fix

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
+function GeneratePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  if (passwordLength < 8) {
+    alert("Password must be at least 8 characters");
+  }
+  if (passwordLength > 128) {
+    alert("Password must be less than 128 characters");
+  }
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    alert("Password must be between 8 and 128 characters");
+  }
+  if (passwordLowercase === false && passwordUppercase === false && passwordNumeric === false && passwordSpecial === false) {
+    alert("You must select at least one character type");
+  }
   for (let i = 1; i < passwordLength; i++) {
     var char = passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
     var password = "";
@@ -13,8 +24,17 @@ function writePassword() {
     var numericCharacters = "0123456789";
     var specialCharacters = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
     passwordCharacters += randomLowercase;
+    
   }
-    class generateBtn { onclick = "writePassword()" }
+    class generatePassword {
+      constructor(passwordLength, passwordLowercase, passwordUppercase, passwordNumeric, passwordSpecial) {
+        this.passwordLength = passwordLength;
+        this.passwordLowercase = passwordLowercase;
+        this.passwordUppercase = passwordUppercase;
+        this.passwordNumeric = passwordNumeric;
+        this.passwordSpecial = passwordSpecial;
+      }
+    }
 
 
 
@@ -23,23 +43,10 @@ function writePassword() {
 
 }
 
-generateBtn.addEventListener("click", writePassword);
-var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password");
+generateBtn.addEventListener("click", GeneratePassword);
 var passwordLength = prompt("How many characters would you like your password to contain?");
 var passwordLength = parseInt(passwordLength);
 var passwordLowercase = confirm("Click OK to confirm including lowercase characters.");
 var passwordUppercase = confirm("Click OK to confirm including uppercase characters.");
 var passwordNumeric = confirm("Click OK to confirm including numeric characters.");
 var passwordSpecial = confirm("Click OK to confirm including special characters.");
-var password = "";
-//var passwordCharacters = "";
-//var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
-///var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//var numericCharacters = "0123456789";
-//var specialCharacters = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
-//var randomLowercase = lowercaseCharacters[Math.floor(Math.random() * lowercaseCharacters.length)];
-//var randomUppercase = uppercaseCharacters[Math.floor(Math.random() * uppercaseCharacters.length)];
-//var randomNumeric = numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
-//var randomSpecial = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-
