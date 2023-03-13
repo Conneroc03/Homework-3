511c377e26877d9ec35d14a278be96b6bf51ce48
Conneroc03 <122755260+Conneroc03@users.noreply.github.com>
2023-03-13 16:27:55 -0600

    working on button

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+  for (let i = 1; i < passwordLength; i++) {
+    var char = passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    var password = "";
+    var passwordCharacters = "";
+    var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+    var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numericCharacters = "0123456789";
+    var specialCharacters = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+    passwordCharacters += randomLowercase;
+  }
+    class generateBtn { onclick = "writePassword()" }
 
 
 
@@ -23,13 +33,13 @@ var passwordUppercase = confirm("Click OK to confirm including uppercase charact
 var passwordNumeric = confirm("Click OK to confirm including numeric characters.");
 var passwordSpecial = confirm("Click OK to confirm including special characters.");
 var password = "";
-var passwordCharacters = "";
-var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericCharacters = "0123456789";
-var specialCharacters = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
-var randomLowercase = lowercaseCharacters[Math.floor(Math.random() * lowercaseCharacters.length)];
-var randomUppercase = uppercaseCharacters[Math.floor(Math.random() * uppercaseCharacters.length)];
-var randomNumeric = numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
-var randomSpecial = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+//var passwordCharacters = "";
+//var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+///var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//var numericCharacters = "0123456789";
+//var specialCharacters = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+//var randomLowercase = lowercaseCharacters[Math.floor(Math.random() * lowercaseCharacters.length)];
+//var randomUppercase = uppercaseCharacters[Math.floor(Math.random() * uppercaseCharacters.length)];
+//var randomNumeric = numericCharacters[Math.floor(Math.random() * numericCharacters.length)];
+//var randomSpecial = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
 
